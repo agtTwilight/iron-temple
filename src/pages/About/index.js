61875509b3aded73711afc2d48fc446e7db1e700
agtTwilight <lukasmacmillen@gmail.com>
2023-08-18 11:06:16 -0700

    added nav highlight to currently viewed component

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -7,12 +7,40 @@ import './style.css';
 const About = () => {
 	const [navValue, setNavValue] = useState('about');
 	const [viewComponent, setViewComponent] = useState(<IronTemple />);
+
+	useEffect(() => {
+		let nav = document.getElementById('about-nav');
+		for (const child of nav.children) {
+			child.classList.remove('active-nav');
+		}
+
+		document.getElementById(`nav-${navValue}`).classList.add('active-nav');
+	}, [navValue]);
+
+	const navHandler = (navVal) => {
+		if (navVal !== navValue) {
+			setNavValue(navVal);
+			if (navVal === 'about') {
+				setViewComponent(<IronTemple />);
+			} else if (navVal === 'team') {
+				setViewComponent(<Team />);
+			} else if (navVal === 'facilities') {
+				setViewComponent(<Facilities />);
+			}
+		}
+	};
 	return (
 		<div>
 			<nav id="about-nav">
-				<h3 onClick={setViewComponent(<IronTemple />)}>About</h3>
-				<h3 onClick={setViewComponent(<Team />)}>Our Team</h3>
-				<h3 onClick={setViewComponent(<Facilities />)}>Our Facilities</h3>
+				<h3 id="nav-about" onClick={() => navHandler('about')}>
+					About
+				</h3>
+				<h3 id="nav-team" onClick={() => navHandler('team')}>
+					Our Team
+				</h3>
+				<h3 id="nav-facilities" onClick={() => navHandler('facilities')}>
+					Our Facilities
+				</h3>
 			</nav>
 			{viewComponent}
 		</div>
