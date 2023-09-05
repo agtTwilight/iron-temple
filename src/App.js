b5c572621480f8006d95adbebca2fd291da1ad67
agtTwilight <lukasmacmillen@gmail.com>
2023-09-05 11:03:48 -0700

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './pages/Home';
-import About from './pages/About';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
 	return (
-		<div className="App">
-			<Home />
-			<About />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/about" element={<About />}></Route>
+				<Route path="/memberships"></Route>
+				<Route path="/store"></Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
